a23286285aa94d10cdc6866eeef5d1ea1e246146
Wilson Giovanny Velandia Barreto <willyv78@gmail.com>
2017-04-05 14:12:16 -0500

    Actualización 03042017
    Pestañas de la sección servicios

--- js/funtions.js
+++ js/funtions.js
@@ -1,4 +1,10 @@
 // Mis funciones personalizadas
+function selectTab(argument) {
+    var id_panel = $(this).find(".nav-link").attr("href");
+    $(".nav-link").removeClass("active");
+    $(".tab-pane").removeClass("show");
+    $(id_panel).addClass("show");
+}
 
 function linkActivo(argument) {
     $(".nav-item").removeClass("active");
@@ -7,6 +13,7 @@ function linkActivo(argument) {
 
 function cargarInicio(argument) {
     $(".nav-item").on("click", linkActivo);
+    $(".nav-item").on("click", selectTab);
 }
 
 $(function() {
