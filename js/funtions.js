e9e2c58b624fc4f5549e867809b387b2b9a9ffb5
Wilson Giovanny Velandia Barreto <willyv78@gmail.com>
2017-04-06 23:38:01 -0500

    Servicios y nosotros
    Se crea página servicios y nosotros.

--- js/funtions.js
+++ js/funtions.js
@@ -1,19 +1,19 @@
 // Mis funciones personalizadas
 function selectTab(argument) {
     var id_panel = $(this).find(".nav-link").attr("href");
-    $(".nav-link").removeClass("active");
-    $(".tab-pane").removeClass("show");
+    $(".tabs-4 > .nav-link").removeClass("active");
+    $(".tab-content > .tab-pane").removeClass("show");
     $(id_panel).addClass("show");
 }
 
 function linkActivo(argument) {
-    $(".nav-item").removeClass("active");
+    $(".tabs-4 > .nav-item").removeClass("active");
     $(this).addClass("active");
 }
 
 function cargarInicio(argument) {
-    $(".nav-item").on("click", linkActivo);
-    $(".nav-item").on("click", selectTab);
+    $(".tabs-4 > .nav-item").on("click", linkActivo);
+    $(".tabs-4 > .nav-item").on("click", selectTab);
 }
 
 $(function() {
